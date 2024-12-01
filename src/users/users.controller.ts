e112fb88d83d0f19fa60a67014b9d9df8d52ac30
Shantanu Lad <shaan.lad@gmail.com>
2024-12-01 23:50:18 +0530

    Added logic for image upload

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,23 @@
 import {
   Body,
   Controller,
+  FileTypeValidator,
   Get,
+  MaxFileSizeValidator,
+  ParseFilePipe,
   Post,
+  UploadedFile,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
 import { UsersService } from './users.service';
-import { NoFilesInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, NoFilesInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import { TokenPayload } from 'src/auth/token-payload.interface';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('users')
 export class UsersController {
@@ -34,4 +40,28 @@ export class UsersController {
   async getUsers() {
     return this.usersService.getUsers();
   }
+
+  @Post(':userId/image')
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: 'public/users',
+        filename: (req, file, callback) => {
+          callback(null, `${req.params.userId}${extname(file.originalname)}`);
+        },
+      }),
+    }),
+  )
+  uploadUserImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          //   new MaxFileSizeValidator({ maxSize: 50000000 }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+        ],
+      }),
+    )
+    _file: Express.Multer.File,
+  ) {}
 }
