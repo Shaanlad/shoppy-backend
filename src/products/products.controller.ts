800183d5e33824eaac3cf87665f20d91ace6e70e
Shantanu Lad <shaan.lad@gmail.com>
2026-07-26 21:28:50 +0530

    Added Route for Product Image Upload

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,23 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CreateProductRequest } from './dto/create-product.request';
 import { CurrentUser } from '../auth/current-user.decorator';
 import { TokenPayload } from '../auth/token-payload.interface';
 import { ProductsService } from './products.service';
+import { diskStorage } from 'multer';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { extname } from 'path';
 
 @Controller('products')
 export class ProductsController {
@@ -23,4 +37,32 @@ export class ProductsController {
   async getAllProducts() {
     return this.productsService.getAllProducts();
   }
+
+  @Post(':productId/image')
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: 'public/products',
+        filename: (req, file, callback) => {
+          callback(
+            null,
+            `${req.params.productId}${extname(file.originalname)}`,
+          );
+        },
+      }),
+    }),
+  )
+  uploadProductImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          //upload limit 10 mb filesize
+          new MaxFileSizeValidator({ maxSize: 10000000 }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+        ],
+      }),
+    )
+    _file: Express.Multer.File,
+  ) {}
 }
